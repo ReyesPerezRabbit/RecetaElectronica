d336729d16165b033e3f137b50111c260f43a08e
ReyesPerezRabbit <102281476+ReyesPerezRabbit@users.noreply.github.com>
2022-06-11 22:06:04 -0500

    Create conexion.php

---
+++ php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$conexion =mysqli_connect("localhost","root","","login_registro_receta");
+
+
+
+/*
+if($conexion) {
+    echo 'Conectado a la base de datos';
+}else{
+    echo 'no se pudo Conectar a la base de datos';
+}*/
+?>
