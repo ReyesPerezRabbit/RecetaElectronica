097d69f92c655667997cc01c35400d0868f9e83c
ReyesPerezRabbit <102281476+ReyesPerezRabbit@users.noreply.github.com>
2022-06-11 22:00:00 -0500

    Create formSintomas.php

---
+++ formSintomas.php
@@ -0,0 +1,36 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Formulario Estudios</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/estiloformularios.css">
+</head>
+<body> 
+    <form method="POST" >
+        <h1>Formulario De Sintomas</h1>
+
+        Nombre Completo:<input type="text" name="nombrecompleto" placeholder="Nombre Completo" >
+
+        Digite su CURP:<input type="text" name="curp" placeholder=" Digite su CURP" >
+
+        Cual es su Edad:<input type="text" name="edad" placeholder="Cual es su Edad" >
+
+        Malestares y Tiempo de los Sintomas:<input type="text" name="tiempomalestar" placeholder="Malestares y Tiempo de los Sintomas" >
+
+        Lugar de la molestia:<input type="text" name="lugarmolestia" placeholder="Lugar de la molestia" >
+
+        Cual es su Direccion:<input type="text" name="direccion" placeholder="Cual es su Direccion" >
+
+        <input type="submit" name="Guardar">
+       
+    </form>
+    <?php
+    include("php/registro_Sintomas.php");
+    ?>
+</body>
+</html>
+
+
