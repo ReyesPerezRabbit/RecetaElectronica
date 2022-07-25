5918ac71cb4ad876f5b6a9009e2232624f85195a
ReyesPerezRabbit <102281476+ReyesPerezRabbit@users.noreply.github.com>
2022-07-24 23:53:42 -0500

    Update registro_Sintomas.php

--- php/registro_Sintomas.php
+++ php/registro_Sintomas.php
@@ -7,21 +7,21 @@ if (isset($_POST['Guardar'])) {
     if (strlen($_POST['nombrecompleto']) >= 1 && 
 	strlen($_POST['curp']) >= 1 &&
     strlen($_POST['edad']) >= 1 &&
-    strlen($_POST['tiempomalestar']) >= 1 &&
+    strlen($_POST['tipo_malestar']) >= 1 &&
     strlen($_POST['lugarmolestia']) >= 1 &&
 	strlen($_POST['direccion']) >= 1  ) {
 
 	    $nombrecompleto = trim($_POST['nombrecompleto']);
 	    $curp = trim($_POST['curp']);
         $edad = trim($_POST['edad']);
-        $tiempomalestar = trim($_POST['tiempomalestar']);
+        $tipo_malestar = trim($_POST['tipo_malestar']);
         $lugarmolestia = trim($_POST['lugarmolestia']);
         $direccion = trim($_POST['direccion']);
 
 	    $fecha_reg = date("d/m/y");
 
-	    $consulta = "INSERT INTO formsintomas( nombrecompleto, curp, edad, tiempomalestar, lugarmolestia, direccion, fecha_reg) 
-        VALUES (' $nombrecompleto','  $curp',' $edad','  $tiempomalestar ','  $lugarmolestia','   $direccion',' $fecha_reg ')";
+	    $consulta = "INSERT INTO formsintomas ( nombrecompleto, curp, edad, tipo_malestar, lugarmolestia, direccion, fecha_reg) 
+        VALUES (' $nombrecompleto','  $curp',' $edad','  $tipo_malestar ','  $lugarmolestia','   $direccion',' $fecha_reg ')";
 
 	    $resultado = mysqli_query($conexion,$consulta);
 		
